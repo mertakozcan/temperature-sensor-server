e055138e6be9a9e1ca90f8211ee0540ea69bc493
Mert <mertakozcan@gmail.com>
2018-01-24 11:46:39 +0300

    Fix sorting bug

--- app/controllers/sensor_data_controller.rb
+++ app/controllers/sensor_data_controller.rb
@@ -1,7 +1,8 @@
 class SensorDataController < ApplicationController
 
   def index
-      @sensor_data = SensorDatum.all
+    @sensor_data = SensorDatum.all
+    @sorted_sensor_data = SensorDatum.order(created_at: :desc)
   end
 
 end
