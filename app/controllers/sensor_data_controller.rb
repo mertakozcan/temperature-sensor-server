c4da5709037d61f958b0f0e5d388bd2ea2f980ff
Mert <mertakozcan@gmail.com>
2019-01-17 15:28:42 +0300

    Implement download as CSV

--- app/controllers/sensor_data_controller.rb
+++ app/controllers/sensor_data_controller.rb
@@ -1,8 +1,15 @@
 class SensorDataController < ApplicationController
 
   def index
-    @sensor_data = SensorDatum.all
-    @sorted_sensor_data = SensorDatum.order(created_at: :desc)
+    @sensor_data = SensorDatum.order(created_at: :desc)
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        date = request.query_parameters['date'] or Time.now.months_ago(1).strftime('%Y-%m')
+        send_data SensorDatum.to_csv(date), filename: "sensor-#{date}.csv"
+      }
+    end
   end
 
 end
--- app/models/sensor_datum.rb
+++ app/models/sensor_datum.rb
@@ -1,3 +1,21 @@
+require 'csv'
+
 class SensorDatum < ApplicationRecord
   validates :temperature, :humidity, presence: true
+
+  def self.to_csv(date)
+    attributes = %w{Tarih Saat Sicaklik Nem}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      date_obj = Date::strptime(date, "%Y-%m")
+      all.where(:created_at => date_obj.beginning_of_month..date_obj.end_of_month).each do |datum|
+        csv << [datum.created_at.localtime.strftime('%d/%m/%y'),
+                datum.created_at.localtime.strftime('%H:%M'),
+                datum.temperature,
+                datum.humidity]
+      end
+    end
+  end
 end
