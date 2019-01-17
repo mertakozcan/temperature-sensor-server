2b9bd7be3657515eacc45c585f9a96dc01437922
Mert <mertakozcan@gmail.com>
2019-01-17 14:10:00 +0300

    Update ESP8266 code

--- ESP8266/temperature-humidity-sensor.ino
+++ ESP8266/temperature-humidity-sensor.ino
@@ -1,96 +1,69 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
+#include <DNSServer.h>
 #include <ESP8266HTTPClient.h>
-#include <DHT.h>
-#include <time.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h> // WiFi Setup
+#include "DHT.h"
 
-#define DHT_PIN 12 // D6 pin on ESP8266 NodeMCU
-#define DHT_TYPE DHT22
+// Define DHT pin
+#define DHTPIN 14 // D5 on board
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
-ESP8266WiFiMulti WiFiMulti;
+// Define LED pin
+#define REDPIN 13 // D7 on board
 
-// Create a sensor instance
-DHT dht(DHT_PIN, DHT_TYPE);
-
-// WiFi login information
-char ssid[] = "mert";
-char password[] = "24681012";
-
-bool read_data = true;
+// Define HTTP Client
+HTTPClient http;
 
 void setup() {
-  //Serial.begin(115200);
-  //Serial.setDebugOutput(true);
-  //Serial.println();
-  //Serial.println();
-  //Serial.println();
-  for(uint8_t t = 4; t > 0; t--) {
-    //Serial.printf("[SETUP] WAIT %d...\n", t);
-    //Serial.flush();
-    delay(1000);
-  }
+  Serial.begin(115200);
 
-  // Configure WiFi
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(ssid, password);
+  pinMode(REDPIN, OUTPUT);
 
-  // Configure time server
-  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  digitalWrite(REDPIN, HIGH); // turn on LED
+
+  // Put ESP8266 in server mode and allow user to communicate with board
+  // over WiFi and select the network of his/her choice to connect
+  WiFiManager wifiManager;
+  // wifiManager.resetSettings(); // For testing only
+  wifiManager.autoConnect("KURULUM");
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  digitalWrite(REDPIN, LOW); // turn off LED
+
+  // Start DHT sensor
+  dht.begin();
 }
 
 void loop() {
-  // Wait for WiFi connection
-  if((WiFiMulti.run() == WL_CONNECTED)) {
-    // Get current time
-    time_t now;
-    struct tm *timeinfo;
-    time(&now);
-    timeinfo = localtime(&now);
-    int current_hour = timeinfo->tm_hour;
-    int current_minute = timeinfo->tm_min;
-    //Serial.printf("Current time: %d:%d\n", current_hour, current_minute);
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  Serial.printf("Temperature: %f Humidity: %f\n", temperature, humidity);
 
-    if (current_hour == 4 && current_minute < 5) {
-      read_data = true;
-    } else if (current_hour == 9 && current_minute < 5) {
-      read_data = true;
-    } else if (current_hour == 13 && current_minute < 5) {
-      read_data = true;
-    } else if (current_hour == 16 && current_minute < 5) {
-      read_data = true;
-    } else if (current_hour == 20 && current_minute < 5) {
-      read_data = true;
-    } else if (current_hour == 23 && current_minute < 5) {
-      read_data = true;
+  if (!isnan(temperature) && !isnan(humidity)) {
+    digitalWrite(REDPIN, LOW); // reset LED
+
+    // Make HTTP request
+    Serial.println("[HTTP] Begin...");
+    String requestUri = String("/api/v1/sensor_data?temperature=") + String(temperature) + String("&humidity=") + String(humidity);
+    http.begin("akozcan.herokuapp.com", 80, requestUri);
+    int http_code = http.POST("");
+    Serial.printf("[HTTP] POST... code: %d\n", http_code);
+    http.end();
+
+    // Evaluate HTTP response
+    if(http_code == 200 || http_code == 201) {
+      delay(300000); // 5 minutes
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(http_code).c_str());
     }
-
-    if (read_data) {
-      HTTPClient http;
-
-      float temperature = dht.readTemperature();
-      float humidity = dht.readHumidity();
-      //Serial.printf("Temperature: %f Humidity: %f\n", temperature, humidity);
-
-      //Serial.println("[HTTP] Begin...");
-      String requestUri = String("/api/v1/sensor_data?temperature=") + String(temperature) + String("&humidity=") + String(humidity);
-      http.begin("temperature-sensor-app.herokuapp.com", 80, requestUri);
-      
-      //Serial.println("[HTTP] POST...");
-      int http_code = http.POST("");
-      //Serial.printf("[HTTP] POST... code: %d\n", http_code);
-      if(http_code == 200 || http_code == 201) {
-        String payload = http.getString();
-        //Serial.println(payload);
-        read_data = false;
-      } else {
-        //Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(http_code).c_str());
-        read_data = true;
-      }
-      http.end();
-    }
+  } else {
+    digitalWrite(REDPIN, HIGH); // turn on LED to indicate the problem
   }
-  // 5 minutes
-  delay(300000);
-}
 
+}
