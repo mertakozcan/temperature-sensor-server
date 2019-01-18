332f5b88951b583c61b3dd1035b5cacfdde73880
Mert <mertakozcan@gmail.com>
2019-01-18 11:18:54 +0300

    Change timezone to Istanbul

--- app/models/sensor_datum.rb
+++ app/models/sensor_datum.rb
@@ -11,8 +11,8 @@ class SensorDatum < ApplicationRecord
 
       date_obj = Date::strptime(date, "%Y-%m")
       all.where(:created_at => date_obj.beginning_of_month..date_obj.end_of_month).each do |datum|
-        csv << [datum.created_at.localtime.strftime('%d/%m/%y'),
-                datum.created_at.localtime.strftime('%H:%M'),
+        csv << [datum.created_at.in_time_zone('Istanbul').strftime('%d/%m/%y'),
+                datum.created_at.in_time_zone('Istanbul').strftime('%H:%M'),
                 datum.temperature,
                 datum.humidity]
       end
